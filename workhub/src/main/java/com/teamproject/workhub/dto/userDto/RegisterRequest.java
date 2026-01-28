19afb4fb71f4c17123e99e33ba19db771f9558bd
rjswh0503 <rjswh0503@yahoo.com>
2026-01-28 18:21:59 +0900

    asd

--- workhub/src/main/java/com/teamproject/workhub/dto/userDto/RegisterRequest.java
+++ workhub/src/main/java/com/teamproject/workhub/dto/userDto/RegisterRequest.java
@@ -1,4 +1,21 @@
 package com.teamproject.workhub.dto.userDto;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
 public class RegisterRequest {
+    private String employeeNo;
+    private String name;
+
+    private String password;
+    private Long departNo;
+    private String email;
+    private String phone;
+    private String position;
+    private LocalDate joinDate;
 }
--- workhub/src/main/java/com/teamproject/workhub/repository/userRepository/UserRepository.java
+++ workhub/src/main/java/com/teamproject/workhub/repository/userRepository/UserRepository.java
@@ -1,4 +1,13 @@
 package com.teamproject.workhub.repository.userRepository;
 
-public class UserRepository {
+import com.teamproject.workhub.entity.userEntity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmployeeNo(String employeeNo);
+
+    boolean existsByEmployeeNo(String employeeNo);
+
 }
