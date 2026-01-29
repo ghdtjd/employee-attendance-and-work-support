5b18a15c962efcdeb057966718d22fb599db64bb
rjswh0503 <rjswh0503@yahoo.com>
2026-01-29 13:20:16 +0900

    logout

--- workhub/src/main/java/com/teamproject/workhub/controller/userController/UserController.java
+++ workhub/src/main/java/com/teamproject/workhub/controller/userController/UserController.java
@@ -2,6 +2,7 @@ package com.teamproject.workhub.controller.userController;
 
 import com.teamproject.workhub.dto.userDto.LoginRequest;
 import com.teamproject.workhub.dto.userDto.RegisterRequest;
+import com.teamproject.workhub.entity.employeeEntity.Employee;
 import com.teamproject.workhub.entity.userEntity.User;
 import com.teamproject.workhub.repository.EmployeeRepository.EmployeeRepository;
 import com.teamproject.workhub.service.userService.UserService;
@@ -12,10 +13,11 @@ import org.apache.coyote.Response;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -50,6 +52,48 @@ public class UserController {
         return ResponseEntity.ok("로그인 성공!");
 
     }
+
+
+    // 로그아웃
+    @PostMapping("/logout")
+    public ResponseEntity<String> logout(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if(session != null ) session.invalidate();
+
+        return ResponseEntity.ok("로그아웃 성공!!!");
+
+    }
+
+
+    @GetMapping("/check-login")
+    public ResponseEntity<Map<String, Object>> checkLogin(HttpServletRequest request) {
+
+
+        HttpSession session = request.getSession(false);
+
+        Object loginUserObj = session.getAttribute("loginUser");
+
+        User user = (User) loginUserObj;
+
+        Employee employee = employeeRepository.findByUser(user)
+                .orElseThrow(() -> new RuntimeException("사원 정보를 찾을 수 없습니다."));
+
+
+        Map<String, Object> response = new HashMap<>();
+
+            response.put("employeeNo", user.getEmployeeNo());
+            response.put("role", user.getRole());
+            response.put("name", employee.getName());
+            response.put("email", employee.getEmail());
+            response.put("phone", employee.getPhone());
+            response.put("position", employee.getPosition());
+
+            return ResponseEntity.ok(response);
+
+    }
+
+
+
 
 
 
