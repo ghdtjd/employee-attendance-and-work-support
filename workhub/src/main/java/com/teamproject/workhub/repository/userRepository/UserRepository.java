fcf8832316d12e1a996333e9c83895ef5954d326
rjswh0503 <rjswh0503@yahoo.com>
2026-01-30 18:21:00 +0900

    asd

--- workhub/src/main/java/com/teamproject/workhub/repository/userRepository/UserRepository.java
+++ workhub/src/main/java/com/teamproject/workhub/repository/userRepository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.teamproject.workhub.repository.userRepository;
 
 import com.teamproject.workhub.entity.userEntity.User;
+import org.springframework.data.domain.Limit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -9,5 +10,10 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmployeeNo(String employeeNo);
 
     boolean existsByEmployeeNo(String employeeNo);
+
+    // 비밀번호 초기화 설정
+
+
+
 
 }
